607c854d22d9ee816ec7be2255d1bbd12221f94e
mmerrill <mmerrill@10.111.33.223>
2017-02-22 10:08:42 -0500

    checkpointing

---
+++ job.go
@@ -0,0 +1,110 @@
+/*
+Copyright 2016 Mike Merrill All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"github.com/golang/glog"
+	"github.com/prometheus/client_golang/prometheus"
+	"golang.org/x/net/context"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api"
+	"k8s.io/client-go/pkg/apis/batch/v1"
+	"k8s.io/client-go/tools/cache"
+)
+
+var (
+	descJobStatus = prometheus.NewDesc(
+		"kube_job_status",
+		"The last run status of a job",
+		[]string{"namespace", "name"}, nil,
+	)
+	descJobStartTime = prometheus.NewDesc(
+		"kube_job_status_time",
+		"The start time for a job",
+		[]string{"namespace", "name"}, nil,
+	)
+	descJobCompletionTime = prometheus.NewDesc(
+		"kube_job_completion_time",
+		"The completion time for a job",
+		[]string{"namespace", "name"}, nil,
+	)
+)
+
+type JobsLister func() ([]v1.Job, error)
+
+func (l JobsLister) List() ([]v1.Job, error) {
+	return l()
+}
+
+func RegisterJobsCollector(registry prometheus.Registerer, kubeClient kubernetes.Interface) {
+	client := kubeClient.Extensions().RESTClient()
+	jlw := cache.NewListWatchFromClient(client, "jobs", api.NamespaceAll, nil)
+	jinf := cache.NewSharedInformer(jlw, &[]v1.Job{}, resyncPeriod)
+
+	jLister := JobsLister(func() (jobList v1.JobList, err error) {
+		for _, c := range jinf.GetStore().List() {
+			jobs = append(jobs, *(c.(*v1.Job)))
+		}
+		return jobs, nil
+	})
+
+	registry.MustRegister(&jobsCollector{store: jLister})
+	go jinf.Run(context.Background().Done())
+}
+
+type jobsStore interface {
+	List() (jobs []v1.Job, err error)
+}
+
+// jobsCollector collects metrics about all jobs in the cluster.
+type jobsCollector struct {
+	store jobsStore
+}
+
+// Describe implements the prometheus.Collector interface.
+func (jc *jobsCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- descJobStatus
+	ch <- descJobTime
+}
+
+// Collect implements the prometheus.Collector interface.
+func (jc *jobsCollector) Collect(ch chan<- prometheus.Metric) {
+	jls, err := jc.store.List()
+	if err != nil {
+		glog.Errorf("listing jobs failed: %s", err)
+		return
+	}
+	for _, j := range jls {
+		jc.collectJobsSet(ch, j)
+	}
+}
+
+func (jc *jobsCollector) collectJobs(ch chan<- prometheus.Metric, j v1.Job) {
+	addGauge := func(desc *prometheus.Desc, v float64, lv ...string) {
+		lv = append([]string{j.Namespace, j.Name}, lv...)
+		ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, v, lv...)
+	}
+	if(j.Status.Active > 0) {
+		addGauge(descJobStatus, 2))
+	} else if(j.Status.Failed > 0) {
+		addGauge(descJobStatus, 0))
+	} else {
+		addGauge(descJobStatus, 1))
+	}
+	addGauge(descJobStartTime, float64(j.Status.StartTime.Unix()))
+	addGauge(descJobCompletionTime, float64(j.Status.CompletionTime.Unix()))
+}
---
+++ main.go
@@ -0,0 +1,254 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/openshift/origin/pkg/util/proc"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/spf13/pflag"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	resyncPeriod = 5 * time.Minute
+	metricsPath  = "/metrics"
+	healthzPath  = "/healthz"
+)
+
+var (
+	defaultCollectors = collectorSet{
+		"jobs": struct{}{},
+	}
+	availableCollectors = map[string]func(registry prometheus.Registerer, kubeClient clientset.Interface){
+		"jobs": RegisterJobsCollector,
+	}
+)
+
+type collectorSet map[string]struct{}
+
+func (c *collectorSet) String() string {
+	s := *c
+	return strings.Join(s.asSlice(), ",")
+}
+
+func (c *collectorSet) Set(value string) error {
+	s := *c
+	cols := strings.Split(value, ",")
+	for _, col := range cols {
+		_, ok := availableCollectors[col]
+		if !ok {
+			glog.Fatalf("Collector \"%s\" does not exist", col)
+		}
+		s[col] = struct{}{}
+	}
+	return nil
+}
+
+func (c collectorSet) asSlice() []string {
+	cols := []string{}
+	for col, _ := range c {
+		cols = append(cols, col)
+	}
+	return cols
+}
+
+func (c collectorSet) isEmpty() bool {
+	return len(c.asSlice()) == 0
+}
+
+func (c *collectorSet) Type() string {
+	return "map[string]struct{}"
+}
+
+type options struct {
+	inCluster  bool
+	apiserver  string
+	kubeconfig string
+	help       bool
+	port       int
+	collectors collectorSet
+}
+
+func main() {
+	// configure glog
+	flag.CommandLine.Parse([]string{})
+	flag.Lookup("logtostderr").Value.Set("true")
+
+	options := &options{collectors: make(collectorSet)}
+	flags := pflag.NewFlagSet("", pflag.ExitOnError)
+
+	flags.BoolVar(&options.inCluster, "in-cluster", true, `If true, use the built in kubernetes cluster for creating the client`)
+	flags.StringVar(&options.apiserver, "apiserver", "", `The URL of the apiserver to use as a master`)
+	flags.StringVar(&options.kubeconfig, "kubeconfig", "", "Absolute path to the kubeconfig file")
+	flags.BoolVarP(&options.help, "help", "h", false, "Print help text")
+	flags.IntVar(&options.port, "port", 80, `Port to expose metrics on.`)
+	flags.Var(&options.collectors, "collectors", "Collectors to be enabled")
+
+	flags.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flags.PrintDefaults()
+	}
+
+	err := flags.Parse(os.Args)
+	if err != nil {
+		glog.Fatalf("Error: %s", err)
+	}
+
+	if options.help {
+		flags.Usage()
+		os.Exit(0)
+	}
+
+	var collectors collectorSet
+	if len(options.collectors) == 0 {
+		glog.Infof("Using default collectors")
+		collectors = defaultCollectors
+	} else {
+		collectors = options.collectors
+	}
+
+	if isNotExists(options.kubeconfig) && !(options.inCluster) {
+		glog.Fatalf("kubeconfig invalid and --in-cluster is false; kubeconfig must be set to a valid file(kubeconfig default file name: $HOME/.kube/config)")
+	}
+	if options.apiserver != "" {
+		glog.Infof("apiserver set to: %v", options.apiserver)
+	}
+
+	proc.StartReaper()
+
+	kubeClient, err := createKubeClient(options.inCluster, options.apiserver, options.kubeconfig)
+	if err != nil {
+		glog.Fatalf("Failed to create client: %v", err)
+	}
+
+	registerCollectors(kubeClient, collectors)
+	metricsServer(options.port)
+}
+
+func isNotExists(file string) bool {
+	if file == "" {
+		file = clientcmd.NewDefaultClientConfigLoadingRules().GetDefaultFilename()
+	}
+	_, err := os.Stat(file)
+	return os.IsNotExist(err)
+}
+
+func createKubeClient(inCluster bool, apiserver string, kubeconfig string) (kubeClient clientset.Interface, err error) {
+	if inCluster {
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			return nil, err
+		}
+		// Allow overriding of apiserver even if using inClusterConfig
+		// (necessary if kube-proxy isn't properly set up).
+		if apiserver != "" {
+			config.Host = apiserver
+		}
+		tokenPresent := false
+		if len(config.BearerToken) > 0 {
+			tokenPresent = true
+		}
+		glog.Infof("service account token present: %v", tokenPresent)
+		glog.Infof("service host: %s", config.Host)
+		if kubeClient, err = clientset.NewForConfig(config); err != nil {
+			return nil, err
+		}
+	} else {
+		loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+		// if you want to change the loading rules (which files in which order), you can do so here
+		loadingRules.ExplicitPath = kubeconfig
+		configOverrides := &clientcmd.ConfigOverrides{}
+		// if you want to change override values or bind them to flags, there are methods to help you
+		kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+		config, err := kubeConfig.ClientConfig()
+		//config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		//config, err := clientcmd.DefaultClientConfig.ClientConfig()
+		if err != nil {
+			return nil, err
+		}
+		kubeClient, err = clientset.NewForConfig(config)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Informers don't seem to do a good job logging error messages when it
+	// can't reach the server, making debugging hard. This makes it easier to
+	// figure out if apiserver is configured incorrectly.
+	glog.Infof("Testing communication with server")
+	_, err = kubeClient.Discovery().ServerVersion()
+	if err != nil {
+		return nil, fmt.Errorf("ERROR communicating with apiserver: %v", err)
+	}
+	glog.Infof("Communication with server successful")
+
+	return kubeClient, nil
+}
+
+func metricsServer(port int) {
+	// Address to listen on for web interface and telemetry
+	listenAddress := fmt.Sprintf(":%d", port)
+
+	glog.Infof("Starting job metrics server: %s", listenAddress)
+	// Add metricsPath
+	http.Handle(metricsPath, prometheus.UninstrumentedHandler())
+	// Add healthzPath
+	http.HandleFunc(healthzPath, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	})
+	// Add index
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+             <head><title>Job Metrics Server</title></head>
+             <body>
+             <h1>Job Metrics</h1>
+			 <ul>
+             <li><a href='` + metricsPath + `'>metrics</a></li>
+             <li><a href='` + healthzPath + `'>healthz</a></li>
+			 </ul>
+             </body>
+             </html>`))
+	})
+	log.Fatal(http.ListenAndServe(listenAddress, nil))
+}
+
+// registerCollectors creates and starts informers and initializes and
+// registers metrics for collection.
+func registerCollectors(kubeClient clientset.Interface, enabledCollectors collectorSet) {
+	activeCollectors := []string{}
+	for c, _ := range enabledCollectors {
+		f, ok := availableCollectors[c]
+		if ok {
+			f(prometheus.DefaultRegisterer, kubeClient)
+			activeCollectors = append(activeCollectors, c)
+		}
+	}
+
+	glog.Infof("Active collectors: %s", strings.Join(activeCollectors, ","))
+}
