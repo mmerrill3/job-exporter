7e39988c2deb95a3897e85e7c9d1c6ecea016a0a
mmerrill <mmerrill@10.111.99.164>
2017-02-23 17:40:52 -0500

    Updating to use json parsing, only one set of metrics per job

--- src/mmerrill.io/job-exporter/job.go
+++ src/mmerrill.io/job-exporter/job.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 Mike Merrill All rights reserved.
+Copyright 2017 Vonage All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,7 +17,9 @@ limitations under the License.
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/net/context"
 	"k8s.io/client-go/kubernetes"
@@ -27,6 +29,27 @@ import (
 	"os"
 )
 
+type jobAnnotation struct {
+	Kind       string
+	APIVersion string
+	Reference  struct {
+		Kind            string
+		Namespace       string
+		Name            string
+		UID             string
+		APIVersion      string
+		ResourceVersion string
+	}
+}
+
+type jobGauge struct {
+	name           string
+	status         int64
+	startTime      int64
+	completionTime int64
+	namespace      string
+}
+
 var (
 	descJobStatus = prometheus.NewDesc(
 		"kube_job_status",
@@ -34,7 +57,7 @@ var (
 		[]string{"namespace", "name"}, nil,
 	)
 	descJobStartTime = prometheus.NewDesc(
-		"kube_job_status_time",
+		"kube_job_start_time",
 		"The start time for a job",
 		[]string{"namespace", "name"}, nil,
 	)
@@ -43,6 +66,7 @@ var (
 		"The completion time for a job",
 		[]string{"namespace", "name"}, nil,
 	)
+	jobMap = make(map[string]jobGauge)
 )
 
 type JobsLister func() ([]v1.Job, error)
@@ -91,26 +115,73 @@ func (jc *jobsCollector) Collect(ch chan<- prometheus.Metric) {
 		return
 	}
 	for _, j := range jls {
-		jc.collectJobs(ch, j)
+		jc.collectJobs(j)
+	}
+
+	addGauge := func(desc *prometheus.Desc, v float64, jobGauge *jobGauge, lv ...string) {
+		lv = append([]string{jobGauge.namespace, jobGauge.name}, lv...)
+		ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, v, lv...)
+	}
+
+	for _, jobGauge := range jobMap {
+		addGauge(descJobStatus, float64(jobGauge.status), &jobGauge)
+		addGauge(descJobStartTime, float64(jobGauge.startTime), &jobGauge)
+		addGauge(descJobCompletionTime, float64(jobGauge.completionTime), &jobGauge)
 	}
 }
 
-func (jc *jobsCollector) collectJobs(ch chan<- prometheus.Metric, j v1.Job) {
-	addGauge := func(desc *prometheus.Desc, v float64, lv ...string) {
-		lv = append([]string{j.Namespace, j.Name}, lv...)
-		ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, v, lv...)
+func (jc *jobsCollector) collectJobs(j v1.Job) {
+	if nil == j.Annotations || nil == j.Status.CompletionTime {
+		glog.Infof("found job that is either not done or unrecognized: %v", j.Name)
+		return
 	}
-	if j.Status.Active > 0 {
-		addGauge(descJobStatus, 2)
-	} else if j.Status.Failed > 0 {
-		addGauge(descJobStatus, 0)
+	status := jc.findStatus(&j)
+	jobMapKey := jc.findMapKey(&j)
+	if "" == jobMapKey {
+		return
+	}
+	jobGauge, ok := jobMap[jobMapKey]
+	if !ok {
+		nameJSON := j.Annotations["kubernetes.io/created-by"]
+		jobAnnotation, _ := jc.decodeJSON([]byte(nameJSON))
+		jobGauge.name = jobAnnotation.Reference.Name
+		glog.Infof("Found gauge name %s", jobGauge.name)
+		jobGauge.completionTime = j.Status.CompletionTime.Unix()
+		jobGauge.startTime = j.Status.StartTime.Unix()
+		jobGauge.status = status
+		jobGauge.namespace = j.Namespace
+		jobMap[jobMapKey] = jobGauge
 	} else {
-		addGauge(descJobStatus, 1)
+		if jobGauge.completionTime <= j.Status.CompletionTime.Unix() {
+			jobGauge.completionTime = j.Status.CompletionTime.Unix()
+			jobGauge.status = status
+			jobGauge.startTime = j.Status.StartTime.Unix()
+			jobMap[jobMapKey] = jobGauge
+		}
 	}
-	if nil != j.Status.StartTime {
-		addGauge(descJobStartTime, float64(j.Status.StartTime.Unix()))
+}
+
+func (jc *jobsCollector) decodeJSON(bytes []byte) (*jobAnnotation, error) {
+	var annotation jobAnnotation
+	err := json.Unmarshal(bytes, &annotation)
+	if err != nil {
+		glog.Fatalf("Cannot parse annotation %s", err)
 	}
-	if nil != j.Status.CompletionTime {
-		addGauge(descJobCompletionTime, float64(j.Status.CompletionTime.Unix()))
+	return &annotation, nil
+}
+
+func (jc *jobsCollector) findStatus(job *v1.Job) int64 {
+	if job.Status.Active > 0 {
+		return 2
+	} else if job.Status.Failed > 0 {
+		return 1
 	}
+	return 0
+}
+
+func (jb *jobsCollector) findMapKey(job *v1.Job) string {
+	if nil == job.Annotations {
+		return ""
+	}
+	return job.Annotations["name"] + job.Namespace
 }
